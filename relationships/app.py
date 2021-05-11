8b74b656ab055dc144818166d19a5812291bbf88
hjt523 <harry.tindale1997@gmail.com>
2021-05-11 15:03:22 +0100

    right commit

---
+++ relationships/app.py
@@ -0,0 +1,21 @@
+from flask import Flask # Import Flask class
+from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy class
+
+app = Flask(__name__) # create Flask object
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.242.189.33:3306/instance' # Set the connection string to connect to the database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app) # create SQLALchemy object
+
+class Countries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    cities = db.relationship('Cities', backref='country') 
+
+class Cities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), nullable=False)
+
+if __name__=='__main__':
+    app.run(debug==True, host='0.0.0.0')
