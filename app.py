17e30d339cd809c8e6638b8ee5d1e4a392a77ef5
hjt523 <harry.tindale1997@gmail.com>
2021-05-11 11:01:34 +0100

    Thats Numberwang! Added a url / num that returns num^2

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return 'Hello World!'
+@app.route('/numwang/<int:number>')
+def numwang(number):
+    number = number**2
+    number = str(number)
+    return number
 
 @app.route('/about')
 def about():
